ef1630a0222416286b13dd89818ffe2b317e93ab
afei1759 <2213382639@qq.com>
2021-02-08 21:17:05 +0800

    q23_合并K个排序链表

--- src/main/java/hash相关/q1_两数之和/Solution1.java
+++ src/main/java/hash相关/q1_两数之和/Solution1.java
@@ -7,7 +7,7 @@ package hash相关.q1_两数之和;
  */
 class Solution1 {
     public int[] twoSum(int[] nums, int target) {
-        //暴力法：从下标1开始，不断取两个数相加判断是否等于target
+        //暴力法：从下标1开始,不断取两个数相加判断是否等于target
         int[] rs=new int[2];
         for(int start=0;start<nums.length;start++){
             for(int end=start;end<nums.length;end++){
